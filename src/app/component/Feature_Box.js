c11ab36d43250270111c088ae7313a9274c7d134
Ella Wu <ellawu010@gmail.com>
2024-08-30 00:36:13 +1200

    add animation

--- src/app/component/Feature_Box.js
+++ src/app/component/Feature_Box.js
@@ -2,7 +2,7 @@ import "../icon-font.css"
 
 const Feature_Box = ({ FeatureIcon, Heading, Paragraph }) => {
   return (
-    <div className="bg-white opacity-80 p-4 rounded-sm text-center">
+    <div className="bg-white opacity-80 p-4 rounded-sm transition-transform text-center hover:-translate-y-4 hover:scale-103">
       <i className={FeatureIcon}></i>
       <h3 className="text-base font-bold uppercase mb-6">{Heading}</h3>
       <p className="text-base mb-10">{Paragraph}</p>
