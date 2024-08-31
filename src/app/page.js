d5a58217cc20a09e56527c2575d2d01d77e4386c
Ella Wu <ellawu010@gmail.com>
2024-09-01 09:58:29 +1200

    add popup and navigation to page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import Navigation from "./layout/navigation";
 import Header from "./layout/header";
 import Section_About from "./layout/section/section_about";
 import Section_Features from "./layout/section/section_features";
@@ -5,10 +6,12 @@ import Section_Tours from "./layout/section/section_tours";
 import Section_Story from "./layout/section/section_story";
 import Section_Book from "./layout/section/section_book";
 import Footer from "./layout/footer";
+import Popup from "./layout/popup";
 
 export default function Home() {
   return (
     <main className="text-gray-500">
+      <Navigation />
       <Header />
       <Section_About />
       <Section_Features />
@@ -16,6 +19,7 @@ export default function Home() {
       <Section_Story />
       <Section_Book />
       <Footer />
+      <Popup />
     </main>
   );
 }
